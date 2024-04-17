3db9cbab9b7bfb96945b58274549dcf9c18f7cb8
zandercymatics <141044360+zandercymatics@users.noreply.github.com>
2024-04-17 13:42:47 -0600

    Test

--- config/buildAssets.js
+++ config/buildAssets.js
@@ -36,7 +36,7 @@ async function createAssetPaths() {
 
 esbuild
   .build({
-    entryPoints: ['styles/styles.scss', 'js/app.js', 'js/admin.js', 'js/get-gov-home.js'],
+    entryPoints: ['styles/styles.scss', 'js/app.js', 'js/admin.js'],
     entryNames: '[dir]/[name]-[hash]',
     outdir: '_site/assets',
     format: 'iife',
